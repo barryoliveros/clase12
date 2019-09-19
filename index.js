c38467348b20f1d02a38e01e17380fbf6138aa8d
santiago valdivia <barry.oliveros@gmail.com>
2019-09-18 20:50:09 -0500

    clase12

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server = http.createServer(function(req,res ) {
+    res.writeHead(200, {'Content-Type ': 'text/plain'});
+    res.write("okay, this is a response from server clas12");
+    res.end() ;
+})
+server.listen(9000)
